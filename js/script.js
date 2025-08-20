11efe23d60921bf78608ad710b1294a641b2c0b4
Waqas Ali <waqasansari4547@gmail.com>
2025-08-20 10:51:47 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 console.log("Hi welcome to the logic building challenge")
+console.log("Hi welcome to the logic building challenge")
+console.log("Hi welcome to the logic building challenge")
+console.log("Hi welcome to the logic building challenge")
