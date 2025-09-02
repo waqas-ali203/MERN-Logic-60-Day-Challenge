e4a14b022cf1273e2f76d594ed0d54e1093233fa
Waqas Ali <144449914+waqas-ali203@users.noreply.github.com>
2025-09-02 17:16:15 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -106,3 +106,15 @@ delete car.model; // delete property
 
 console.log(car); // { brand: 'Toyota', year: 2023 }
 
+function createCounter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const counter1 = createCounter();
+console.log(counter1()); // 1
+console.log(counter1()); // 2
+console.log(counter1()); // 3
