ff766382c2a7baf3e28b74ad559d50456555ed5a
Waqas Ali <144449914+waqas-ali203@users.noreply.github.com>
2025-09-20 15:28:25 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -120,3 +120,20 @@
 // console.log(result); // Pass
 // console.log(result2); // odd
 
+function isValid(s) {
+  const stack = [];
+  const map = { ")": "(", "}": "{", "]": "[" };
+
+  for (let char of s) {
+    if (char in map) {
+      if (stack.pop() !== map[char]) return false;
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid("()[]{}")); // true
+console.log(isValid("(]"));     // false
